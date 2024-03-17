004bced4ea30dddcbb36af03019790ab15bfac38
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-03-17 18:48:22 +0300

    Update RightColumn.tsx

--- src/components/RightColumn/RightColumn.tsx
+++ src/components/RightColumn/RightColumn.tsx
@@ -22,7 +22,7 @@ const RightColumn = observer(() => {
                 <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2, mt: 2 }}>Ваша корзина</Typography>
                 <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}>Количество товаров: {store.totalItemsCount}</Typography>
                 <Typography variant="body1" sx={{ fontWeight: 'bold', mb: 1 }}>Итого: {store.totalAmount.toFixed(2)} ₽</Typography>
-                <Button variant="contained" onClick={handleOpenModal} sx={{ mt: 2, mb: 2 }}>Оплатить</Button>
+                <Button variant="contained" onClick={handleOpenModal} disabled={store.totalItemsCount === 0} sx={{ mt: 2, mb: 2 }}>Оплатить</Button>
             </Paper>
             <PaymentModal open={isModalOpen} onClose={handleCloseModal} />
         </div>
