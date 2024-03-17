e7e208f7049db93a68ccce6ad17cbf511901b45c
Gaik Artur <141954990+webDevArtur@users.noreply.github.com>
2024-03-17 18:40:21 +0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import { CartItem } from '../store/types';
 
 export const fetchCartItems = async (): Promise<CartItem[]> => {
     try {
-        const response = await axios.get('https://fakestoreapi.com/products');
+        const response = await axios.get('https://fakestoreapi.com/products?limit=3');
         return response.data;
     } catch (error) {
         console.error('Error fetching cart items:', error);
